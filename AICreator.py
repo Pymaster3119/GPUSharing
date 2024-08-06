29a51b500186606e32da41ad2b3f4a7a0ab10761
pymaster9 <adi31tya@icloud.com>
2024-08-05 20:57:15 -0400

    Making my GPUSharer use a dropdown to select AIs and allow for addition of layers

--- AICreator.py
+++ AICreator.py
@@ -18,16 +18,17 @@ def animate_move(frame, target_x, target_y):
 def reorder_frames():
     frames.sort(key=lambda frame: frame.winfo_x())
     for idx, frame in enumerate(frames):
-        animate_move(frame, idx*160, 10)
+        animate_move(frame, idx*320, 10)
 
 class DraggableFrame(Frame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-        self.type = StringVar(value=f"Type {len(frames) + 1}")
-        self.arg1 = StringVar(value=f"Input {len(frames) + 1}")
-        self.arg2 = StringVar(value=f"Output {len(frames) + 1}")
+        self.type = StringVar(value="Linear")
+        self.arg1 = StringVar()
+        self.arg2 = StringVar()
         Label(self, text="Type of layer:").grid(row=0, column=0)
-        Entry(self, textvariable=self.type).grid(row=0, column=1)
+        layertypes = ["Linear", "ReLU", "Conv1d", "Conv2d", "Conv3d", "MaxPool1d", "MaxPool2d", "MaxPool3d"]
+        OptionMenu(self, self.type, *layertypes).grid(row=0, column=1)
         Label(self, text="Input Size:").grid(row=1, column=0)
         Entry(self, textvariable=self.arg1).grid(row=1, column=1)
         Label(self, text="Output Size:").grid(row=2, column=0)
@@ -51,17 +52,22 @@ class DraggableFrame(Frame):
 
     def _lock_to_grid(self):
         x = self.winfo_x()
-        new_x = round(x / 160) * 160
+        new_x = round(x / 320) * 320
         self.place(x=new_x, y=10)
         reorder_frames()
 
 root = Tk()
 frames = []
-for i in range(10):
-    frame = DraggableFrame(root, width = 160, height = 160)
-    frame.place(x=i*160, y=10)
+for idx in range(4):
+    frame = DraggableFrame(root, width = 320, height = 320)
+    frame.grid(row=0, column=idx)
     frames.append(frame)
 
+def addlayer():
+    frame = DraggableFrame(root, width = 320, height = 320)
+    frame.grid(row=0, column=len(frames))
+    frames.append(frame)
+Button(root, text="Add Layer", command=addlayer).grid(row=1, column=0, columnspan=100)
 def savelayers(layers):
     for i in frames:
         layers.append(main.AILayer(i.type, i.arg1, i.arg2))
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import torch.nn as nn
 import torch.optim as optim
 class AILayer():
     def __init__(self, type, arg1, arg2):
-        self.layer = eval("nn." + type + "(" + arg1 + "," + arg2 + ")")
+        self.layer = eval("nn." + type.get() + "(" + arg1.get() + "," + arg2.get() + ")")
